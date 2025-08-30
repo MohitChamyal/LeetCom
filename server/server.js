fdc3fbd0f813ec010ae21050a70fc6d041d43869
Mohit Chamyal <mohitchamyal58@gmail.com>
2025-08-30 12:09:27 +0530

    Prepare for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://leetcom-frontend.vercel.app"],
+  origin: ["http://localhost:5173", "https://leetcom-frontend.vercel.app/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
